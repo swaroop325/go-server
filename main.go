315906385f74fb3b89500c9f97012278494dc396
swaroop_a.s <swaroopanand325@gmail.com>
2021-10-29 02:08:45 +0530

    first get api json response

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
+
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type Response struct {
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
+}
+
+var response = Response{StatusCode: 200, Message: "Successful"}
+
 func main() {
-    //Initialising a Router
-    ginEngine := gin.Default()
-    //Creating a Route handler for GET request with anonymous function as handler function
-    ginEngine.GET("/test", hello)
-    //Serving the application on port number 8080
-    ginEngine.Run()
+	//Initialising a Router
+	ginEngine := gin.Default()
+	//Creating a Route handler for GET request with anonymous function as handler function
+	ginEngine.GET("/v1", v1)
+	ginEngine.GET("/test", hello)
+	//Serving the application on port number 8080
+	ginEngine.Run()
 }
 func hello(context *gin.Context) {
-    context.String(http.StatusOK, "Hello World!")
+	context.String(http.StatusOK, "Hello World!")
+}
+func v1(context *gin.Context) {
+	context.JSON(http.StatusOK, response)
 }
